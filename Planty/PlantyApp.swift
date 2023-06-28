1a310177f3c8b87083f092dbf7b3ff505994b968
Muhammad Rezky <mrezkysulihin@gmail.com>
2023-06-28 17:18:55 +0700

    Initial Commit

---
+++ Planty/PlantyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantyApp.swift
+//  Planty
+//
+//  Created by Muhammad Rezky on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
